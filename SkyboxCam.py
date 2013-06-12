73c6845c76ba387fb70631231135a6edc9c4d738
Chris Langford <chris@hellocld.com>
2013-06-12 14:06:28 -0400

    Removed all class stuff from Skybox code - untested

--- SkyboxCam.py
+++ SkyboxCam.py
@@ -1,70 +1,30 @@
 import bge
 
-class SkyboxCam:
-	def __init__(self, cont):
-		self.cont = cont
-		
-		#the owner is the camera, so let's get that
-		self.gameCam = self.cont.owner
-		
-		#check to see if the skybox prop has anything in it
-		#if it's empty main won't do much of anything
-		if self.gameCam["skybox"] != "":
-			self.hasSkybox = True
-			#check to see if the background scene already exists
-			self.skyboxSceneCreated = False
-			for scene in bge.logic.getSceneList():
-				if scene.name == self.gameCam["skybox"]:
-					self.skyboxSceneCreated = True
-			#if it hasn't been created, create the background scene with an actuator
-			if not self.skyboxSceneCreated:
-				self.aScene = self.cont.actuators["Scene"]
-				self.aScene.scene = self.gameCam["skybox"]
-				self.cont.activate(self.aScene)
-		#if the skybox prop is empty, don't do anything
-		else:
-			self.hasSkybox = False
-		
-		#we can't get the skybox camera during init because the background scene doesn't exist yet
-		#instead we create a boolean indicating the camera hasn't yet been found
-		self.gotSkyboxCam = False
-		
-		
-		
-	def getSkyboxCam(self):
-		#create a skyboxCam variable and assign it null
-		self.skyboxCam = None
-		#get the skybox scene
-		for scene in bge.logic.getSceneList():
-			if scene.name == self.gameCam["skybox"]:
-				self.skyboxCam = scene.active_camera
-		#if the camera wasn't found, skyboxCam will still be None
-		if self.skyboxCam is None:
-			self.gotSkyboxCam = False
-		else:
-			self.gotSkyboxCam = True
-		
-	def update(self):
-		#copy the lens and projection matricies
-		self.skyboxCam.lens = self.gameCam.lens
-		self.skyboxCam.projection_matrix = self.gameCam.projection_matrix
-		
-		#copy the rotation of the cam to the skybox cam
-		#be sure to use worldOrientation, because the cam doesn't rotate always rotate around it's local axis
-		self.skyboxCam.worldOrientation = self.gameCam.worldOrientation
 
-		
-skybox_cam = SkyboxCam(bge.logic.getCurrentController())
-
-def main():
-	#always check to see if the data has been freed, and if so re-init
-	if skybox_cam.cont.invalid:
-		skybox_cam.__init__(bge.logic.getCurrentController())
+def getSkyboxCam(cont):
+	#get the skybox scene
+	for scene in bge.logic.getSceneList():
+		#if the scene is found, take the active camera from the skybox scene and return it
+		if scene.name == cont.owner["skybox"]:
+			return scene.active_camera
+	#if the camera wasn't found, return None
+	return None
 	
-	#only run update if there's a skyboxCam
-	if skybox_cam.hasSkybox:
-		#next we get the skybox camera ONLY if we haven't already found it
-		if not skybox_cam.gotSkyboxCam:
-			skybox_cam.getSkyboxCam()
-		else:
-			skybox_cam.update()
+def updateCameras(gameCam, skyboxCam):
+	#copy the lens and projection matricies
+	skyboxCam.lens = gameCam.lens
+	skyboxCam.projection_matrix = gameCam.projection_matrix
+	
+	#copy the rotation of the cam to the skybox cam
+	#be sure to use worldOrientation, because the cam doesn't rotate always rotate around it's local axis
+	skyboxCam.worldOrientation = gameCam.worldOrientation
+
+def update(cont):
+	#we only want to do stuff if there's something filled in the "skybox" property
+	if cont.owner["skybox"] not "":
+		#get the skybox cam (if it exists)
+		skyboxCam = getSkyboxCam(cont)
+		if skyboxCam not None:
+			#update the cameras!
+			updateCameras(cont.owner, skyboxCam)
+			
